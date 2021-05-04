c6a5360027e31d6dcce3ac8c376ea3c41b12954e
carloschanhk <carloschan2215@gmail.com>
2021-05-04 10:51:39 +0800

    added rocket fetch

---
+++ lib/data/rocket_fetch.dart
@@ -0,0 +1,17 @@
+class RocketFetch {
+  static String rocketFetch = """
+  {
+  rockets {
+    name
+    cost_per_launch
+    company
+    country
+    description
+    success_rate_pct
+    first_flight
+    wikipedia
+    active
+  }
+}
+""";
+}
